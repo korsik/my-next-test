6631d19a8324838baf820648c7c1b62dc3fd93a0
Ilias Logothetis <ilogothetis@icloud.com>
2021-07-02 18:56:38 +0300

    recipe app

---
+++ pages/recipes/[slug].js
@@ -0,0 +1,107 @@
+import { useRouter } from "next/router";
+import { useState } from "react";
+import {
+    sanityClient,
+    urlFor, 
+    usePreviewSubscription, 
+    PortableText} from "../../lib/sanity";
+
+const recipeQuery = `*[_type == "recipe" && slug.current == $slug][0]{
+    _id,
+    name,
+    slug,
+    mainImage,
+    ingredient[]{
+        unit,
+        wholeNumber,
+        fraction,
+        ingredient->{
+            name
+        }
+    },
+    instructions,
+    likes
+}`;
+
+export default function OneRecipe({ data, preview }) {
+
+    const router = useRouter()
+
+    if (router.isFallback) {
+        return (
+            <div>Loading...</div>
+        )
+    }
+    // const { data: recipe } = usePreviewSubscription(recipeQuery, {
+    //     params: { slug: data.recipe?.slug.current },
+    //     initialData: data,
+    //     enabled: preview,
+    // }); 
+    const [likes, setLikes] = useState(data?.recipe?.like);
+
+    const addLike = async () => {
+        const res = await fetch("/api/handle-like", {
+            method: "POST",
+            body: JSON.stringify({ _id: recipe._id }),
+        }).catch((error) => console.log(error))
+
+        const data = await res.json();
+        setLikes(data.likes);
+    };
+
+    const { recipe } = data;
+
+    return (
+        <article className="recipe">
+            <h1>{recipe.name}</h1>
+
+            <button className="like-button" onClick={addLike}>
+                {likes} ❤️
+            </button>
+
+            <main className="content">
+                <img src={urlFor(recipe?.mainImage).url()} alt={recipe.name}/>
+                <div className="breakdown">
+                    <ul className="ingredients"> 
+                        {recipe.ingredient?.map((ingredient) => (
+                             <li key={ingredient._key} className="ingredient">
+                                 {ingredient?.wholeNumber}
+                                 {ingredient?.fraction}
+                                 {"  "}
+                                 {ingredient?.unit}
+                                 <br />
+                                 {ingredient?.ingredient?.name}
+                             </li>
+                        ))}
+                       
+                    </ul>
+                    <PortableText blocks={recipe?.instructions} className="instructions"/>
+                </div>
+            </main>
+        </article>
+    );
+}
+
+export async function getStaticPaths() {
+    const paths = await sanityClient.fetch(
+        `*[_type == "recipe" && defined(slug.current)]{
+            "params": {
+                "slug": slug.current
+            }
+        }`
+    );
+
+    return {
+        paths,
+        fallback: true //if the page does not exists it wot throw a 404 but it will go back on previous page
+    }
+}
+
+export async function getStaticProps({ params }) {
+    const { slug } = params;
+    const recipe = await sanityClient.fetch(recipeQuery, {slug})
+
+    return {
+        props: { data: { recipe }, preview: true }
+    };
+}
